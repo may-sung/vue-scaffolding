26d71113ccb82a192aff36136cca126d5ff1aeea
may-sung <may-sung@naver.com>
2020-07-13 14:47:16 +0900

    feat: App Header, 회원가입 컴포넌트를 추가한다
    header 공통 컴포넌트와 회원가입 페이지 컴포넌트를 추가했다.

---
+++ frontend-v2/src/components/SignupForm.vue
@@ -0,0 +1,52 @@
+<template>
+    <form @submit.prevent="submitForm">
+        <div>
+            <label for="username">id: </label>
+            <input
+                id="username"
+                v-model="username"
+                type="text"
+            >
+        </div>
+        <div>
+            <label for="password">pw: </label>
+            <input
+                id="password"
+                v-model="password"
+                type="text"
+            >
+        </div>
+        <div>
+            <label for="nickname">nickname: </label>
+            <input
+                id="nickname"
+                v-model="nickname"
+                type="text"
+            >
+        </div>
+        <button type="submit">
+            login
+        </button>
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            username: '',
+            password: '',
+            nickname: '',
+        };
+    },
+    methods: {
+        submitForm() {
+            console.log('폼 제출');
+        },
+    },
+};
+</script>
+
+<style>
+
+</style>
--- frontend-v2/src/views/SignupPage.vue
+++ frontend-v2/src/views/SignupPage.vue
@@ -1,10 +1,16 @@
 <template>
-    <div>Signup</div>
+    <div>
+        <SignupForm />
+    </div>
 </template>
 
 <script>
-export default {
+import SignupForm from '@/components/SignupForm.vue';
 
+export default {
+    components: {
+        SignupForm,
+    },
 };
 </script>
 
